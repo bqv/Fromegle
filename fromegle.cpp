73db288f6dcb9c38044e37eb10e2905572e1226e
Tony Olagbaiye <frony0@gmail.com>
2012-10-24 22:27:36 +0100

    Completed Selector

--- fromegle.cpp
+++ fromegle.cpp
@@ -1,39 +1,54 @@
-#include <QDesktopWidget>
-#include <QApplication>
-#include <QPushButton>
-#include <iostream>
-#include <QWidget>
-#include <QIcon>
-
-class Selector : public QWidget
-{
-	Q_OBJECT
-
-	public:
-		Selector(QWidget *parent = 0);
-
-	private slots:
-		void initText();
-};
+#include "selector.h"
 
 Selector::Selector(QWidget *parent) : QWidget(parent)
 {
+	const int X = 16;
+	const int Y = 16;
+	const int W = 128;
+	const int H = 32;
+
 	QPushButton *text = new QPushButton("Text Chat", this);
-	text->setGeometry(50, 40, 150, 30);
+	text->setGeometry(X, Y, W, H-2);
+
+	QPushButton *spye = new QPushButton("Spy Mode", this);
+	spye->setGeometry(X, Y+(H), W, H-2);
+
+	QPushButton *quest = new QPushButton("Question Mode", this);
+	quest->setGeometry(X, Y+(2*H), W, H-2);
+
+	QPushButton *video = new QPushButton("Video Chat", this);
+	video->setGeometry(X, Y+(3*H), W, H-2);
+	video->setEnabled(false);
 
 	connect(text, SIGNAL(clicked()), this, SLOT(initText()));
+	connect(spye, SIGNAL(clicked()), this, SLOT(initSpye()));
+	connect(quest, SIGNAL(clicked()), this, SLOT(initQuest()));
+	connect(video, SIGNAL(clicked()), this, SLOT(initVideo()));
 }
 
 void Selector::initText()
 {
-	;
+	std::cout << "Entering Text Mode..." << std::endl;
+}
+void Selector::initSpye()
+{
+	std::cout << "Entering Spy Mode..." << std::endl;
+}
+void Selector::initQuest()
+{
+	std::cout << "Entering Question Mode..." << std::endl;
+}
+void Selector::initVideo()
+{
+	std::cout << "Entering Video Mode..." << std::endl;
+	std::cout << "ERROR: Not Implemented!" << std::endl;
 }
 
 int main(int argc, char *argv[])
 {
 
-	int WIDTH = 250;
-	int HEIGHT = 150;
+	const int WIDTH = 158;
+	const int HEIGHT = 160;
 
 	QApplication app(argc, argv);
 
